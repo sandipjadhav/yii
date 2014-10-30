cb71d62ae02fae02c93d68a7d6a5ca7e9e02848b
sandipjadhav <sandip.j.1980@gmail.com>
2014-10-30 12:22:30 +0530

    Changes : Added query to Upload a photo for profile

--- motocarmaNBR8/protected/config/sqlChanges.sql
+++ motocarmaNBR8/protected/config/sqlChanges.sql
@@ -8,3 +8,9 @@ ALTER TABLE `SavedCars` ADD CONSTRAINT `FK_DealStatus_SavedCars` FOREIGN KEY ( `
 
 ALTER TABLE `SalesPerson` ADD `Name` VARCHAR( 20 ) NULL DEFAULT NULL AFTER `ID`
 
+
+-- Uplaod a photo 
+INSERT INTO `tbl_profiles_fields` (`id`, `varname`, `title`, `field_type`, `field_size`, `field_size_min`, `required`, `match`, `range`, `error_message`, `other_validator`, `default`, `widget`, `widgetparams`, `position`, `visible`) VALUES
+(5, 'profilePhoto', 'Profile Photo', 'VARCHAR', 255, 0, 0, '', '', '', '{"file":{"allowEmpty":"true","maxFiles":"1","types":"jpg,gif,png,jpeg","safe":"true"}}', '', 'UWfile', '', 0, 3);
+
+ALTER TABLE `tbl_profiles` ADD `profilePhoto` VARCHAR( 255 ) NULL ;
