33ab47cb212c0a9d2e5ceed13e7234e7622bc297
sandipjadhav <sandip.j.1980@gmail.com>
2014-10-31 12:16:31 +0530

    Changes : added query

--- motocarmaNBR8/protected/config/sqlChanges.sql
+++ motocarmaNBR8/protected/config/sqlChanges.sql
@@ -13,4 +13,5 @@ ALTER TABLE `SalesPerson` ADD `Name` VARCHAR( 20 ) NULL DEFAULT NULL AFTER `ID`
 INSERT INTO `tbl_profiles_fields` (`id`, `varname`, `title`, `field_type`, `field_size`, `field_size_min`, `required`, `match`, `range`, `error_message`, `other_validator`, `default`, `widget`, `widgetparams`, `position`, `visible`) VALUES
 (5, 'profilePhoto', 'Profile Photo', 'VARCHAR', 255, 0, 0, '', '', '', '{"file":{"allowEmpty":"true","maxFiles":"1","types":"jpg,gif,png,jpeg","safe":"true"}}', '', 'UWfile', '', 0, 3);
 
-ALTER TABLE `tbl_profiles` ADD `profilePhoto` VARCHAR( 255 ) NULL ;
+ALTER TABLE `tbl_profiles` ADD `profilePhoto` VARCHAR( 255 ) NULL DEFAULT NULL ;
+--ALTER TABLE `tbl_profiles` CHANGE `profilePhoto` `profilePhoto` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
